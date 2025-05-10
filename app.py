648e31665e85ef6275c7555920fd554e839a40b2
AVINASH0052 <86907203+AVINASH0052@users.noreply.github.com>
2025-05-10 09:32:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ st.set_page_config(
 # Initialize NVIDIA client
 client = OpenAI(
     base_url="https://integrate.api.nvidia.com/v1",
-    api_key="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"
+    api_key=API_KEY
 )
 
 # Initialize session state for chat history and user info
